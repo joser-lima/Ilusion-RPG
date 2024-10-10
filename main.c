a53fc7da3c61fdf939b2fb16419489b67edade2e
joser-lima <limajrkk@gmail.com>
2024-10-10 09:41:43 -0300

    sistema de cadastro e login funcional

---
+++ main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char usuario[30];
+    char senha[7];
+    char classe[50];
+    int vida;
+    int nivel;
+    int xp;
+    int mana;
+    int forca;
+    int inteligencia;
+    int destreza;
+    int vitalidade;
+} jogador;
+
+int cadastro();
+int login();
+
+int main(){
+
+    login();
+
+    return 0;
+}
+
+int cadastro(){
+    jogador novojogador;
+
+    printf("Bem vindo desafiante! Como prefere ser chamado? \n");
+    scanf("%s", novojogador.usuario);
+
+    printf("Certo, %s, agora diga sua palavra-passe, para quando voltar, sabermos se é você mesmo. \n", novojogador.usuario);
+    scanf("%s", novojogador.senha);
+
+    novojogador.vida = 100;
+    novojogador.nivel = 1;
+    novojogador.xp = 0;
+    novojogador.mana = 50;
+    novojogador.forca = 10;
+    novojogador.inteligencia = 10;
+    novojogador.destreza = 10;
+    novojogador.vitalidade = 10;
+
+    FILE *arquivo = fopen("jogadores.bin", "ab");
+
+    fwrite(&novojogador, sizeof(jogador), 1, arquivo);
+    fclose(arquivo);
+    printf("Tudo preparado, aventureiro. \n");
+
+    login();
+
+}
+
+int login(){
+    char usuario[30];
+    char senha[7];
+    int encontrado = 0;
+    jogador jogadoratual;
+
+    printf("Bem vindo de volta! Diga-me como é chamado em nossas terras. \n");
+    scanf("%s", usuario);
+
+    printf("Certo, agora me diga sua palavra-passe, apenas para confirmação. \n");
+    scanf("%s", senha);
+
+    FILE *arquivo = fopen("jogadores.bin", "rb");
+
+    while(fread(&jogadoratual, sizeof(jogador), 1, arquivo)){
+        if(strcmp(jogadoratual.usuario, usuario) == 0 && strcmp(jogadoratual.senha, senha) == 0){
+            printf("Seja bem-vindo %s, pronto para continuar sua aventura? \n", usuario);
+            encontrado = 1;
+        }
+    }
+
+    fclose(arquivo); 
+
+    if(!encontrado){
+        printf("Alguma informação não está correta, por favor tente novamente. \n");
+        return 0;
+    }
+
+    return 1;
+}
+
